c86d454abc6a8fb887b01e94d8bc80e0a5f26faa
chucholuco <chucholuco@gmail.com>
2021-07-29 21:47:20 -0500

    Initial commit

---
+++ src/main/java/com/example/demo2/customer/Customer.java
@@ -0,0 +1,59 @@
+package com.example.demo2.customer;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class Customer{
+    private final Long id;
+    @NotBlank(message = "name must be not empty")
+    private final String name;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotBlank(message = "password must be not empty")
+    private final String password;
+
+    @NotBlank(message = "email must be not empty")
+    @Email(message = "email is not correct")
+    private final String email;
+
+    Customer(Long id,
+             String name,
+             String password,
+             String email) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    @JsonProperty("customerId")
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/demo2/customer/CustomerRepo.java
@@ -0,0 +1,7 @@
+package com.example.demo2.customer;
+
+import java.util.List;
+
+public interface CustomerRepo {
+    List<Customer> getCustomers();
+}

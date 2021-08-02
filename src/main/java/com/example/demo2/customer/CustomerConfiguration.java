d6cd5556209b49fc4ca955bcba975650638e0424
chucholuco <chucholuco@gmail.com>
2021-08-02 17:29:58 -0500

    Se agrega funcionalidad para Actuator, y logs.

--- src/main/java/com/example/demo2/customer/CustomerConfiguration.java
+++ src/main/java/com/example/demo2/customer/CustomerConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class CustomerConfiguration {
@@ -12,10 +13,18 @@ public class CustomerConfiguration {
     @Value("${app.useFakeCustomerRepo:false}")
     private Boolean useFakeCustomerRepo;
 
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired
+    private Environment environment;
+
     @Bean
     CommandLineRunner commandLineRunner(){
         return args -> {
             System.out.println("CommandLineRunner hooray");
+            System.out.println(companyName);
+            System.out.println(environment.getProperty("info.app.version"));
         };
     }
 
--- src/main/java/com/example/demo2/customer/CustomerService.java
+++ src/main/java/com/example/demo2/customer/CustomerService.java
@@ -1,6 +1,8 @@
 package com.example.demo2.customer;
 
 import com.example.demo2.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ import java.util.List;
 @Service
 public class CustomerService {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
+
     private final CustomerRepository customerRepository;
 
     @Autowired
@@ -19,12 +23,18 @@ public class CustomerService {
     }
 
     List<Customer> getCustomers(){
+        LOGGER.info("getCustomers was called");
         return customerRepository.findAll();
     }
 
     Customer getCustomer(Long id){
+
         return customerRepository.findById(id).orElseThrow(
-                () -> new NotFoundException("Customer with id " + id + " not found"));
+                () -> {
+                    NotFoundException notFoundException = new NotFoundException("Customer with id " + id + " not found");
+                    LOGGER.error("error in getting customer {}", id, notFoundException);
+                    return notFoundException;
+                });
         /*return getCustomers().stream().filter(customer ->
                 customer.getId().equals(id)).findFirst().orElseThrow(
                         () -> new NotFoundException("Customer with id " + id + " not found"));*/
